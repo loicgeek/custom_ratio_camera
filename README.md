85d5243b3927abffcf775076599b4db176e53d91
loicgeek <loic.ngou98@gmail.com>
2022-10-19 22:16:08 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,66 @@
+<!--
+This README describes the package. If you publish this package to pub.dev,
+this README's contents appear on the landing page for your package.
+
+For information about how to write a good package README, see the guide for
+[writing package pages](https://dart.dev/guides/libraries/writing-package-pages).
+
+For general information about developing packages, see the Dart guide for
+[creating packages](https://dart.dev/guides/libraries/create-library-packages)
+and the Flutter guide for
+[developing packages and plugins](https://flutter.dev/developing-packages).
+-->
+
+This package allow you to have a custom ratio for camera preview using the <a href="https://pub.dev/packages/camera">  camera </a> package, you can the have a square camera preview as you need.
+
+## Features
+
+- A CustomRatioCameraPreview widget
+- A callback function that will give you you all necessary informations to perform cropping
+
+## Getting started
+- Install this package  ` flutter pub add custom_ratio_camera `
+- Follow the <a href="https://pub.dev/packages/camera"> installation steps </a> of the camera package
+
+## Usage
+
+
+
+```dart
+
+  /// onCropData function to get cropping parameters
+  ///
+  ///[dx] and [dy] specify the top left corner of the output rectangle
+  ///
+  /// [w] is the width of the output rectangle
+  ///
+  /// [h] is the height of the output rectangle
+  ///
+  ///
+  /// example: ``` ffmpeg -i in.mp4 -filter:v "crop=w:h:dx:dy" out.mp4 ```
+  return CustomRatioCameraPreview(
+       cameraController: cameraController,
+       expectedRatio: 1/1,
+       onCropData: (dx, dy, h, w) {
+         print(dx);
+         print(dy);
+         print(h);
+         print(w);
+       },
+     );
+```
+
+<div style="position:center">
+    <img src='./imgs/1_1.jpg' width="200">
+    <hr>
+    <img src='./imgs/9_16.jpg' width="200">
+    <hr>
+    <img src='./imgs/16_9.jpg' width="200">
+   
+
+## Additional information
+
+This package does not handle the cropping part, you will have to handle cropping using tools like ffmpeg
+
+
+## Credits
---
+++ lib/custom_ratio_camera.dart
@@ -0,0 +1,222 @@
+library custom_ratio_camera;
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+typedef OnCropDataCallback = void Function(
+  int dx,
+  int dy,
+  int w,
+  int h,
+);
+
+// ignore: must_be_immutable
+class CustomRatioCameraPreview extends StatefulWidget {
+  /// The expected aspect ration
+  ///
+  late double expectedRatio;
+
+  /// the camera controller
+  final CameraController cameraController;
+
+  /// This is the callback function to get cropping parameters
+  ///
+  ///[dx] and [dy] specify the top left corner of the output rectangle
+  ///
+  /// [w] is the width of the output rectangle
+  ///
+  /// [h] is the height of the output rectangle
+  ///
+  ///
+  /// example: ``` ffmpeg -i in.mp4 -filter:v "crop=w:h:dx:dy" out.mp4 ```
+  final void Function(
+    int dx,
+    int dy,
+    int w,
+    int h,
+  )? onCropData;
+  CustomRatioCameraPreview({
+    super.key,
+    double? expectedRatio,
+    required this.cameraController,
+    this.onCropData,
+  }) {
+    if (expectedRatio != null) {
+      this.expectedRatio = expectedRatio;
+    } else {
+      this.expectedRatio = cameraController.value.aspectRatio;
+    }
+  }
+
+  @override
+  State<CustomRatioCameraPreview> createState() =>
+      _CustomRatioCameraPreviewState();
+}
+
+class _CustomRatioCameraPreviewState extends State<CustomRatioCameraPreview> {
+  ///
+  late double cameraH;
+  late double cameraW;
+  late double pageH;
+  late double remainingSpace;
+  late double blackedZone;
+  late double realSizeCoef;
+  Color bgColor = Colors.black;
+
+  @override
+  Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback(
+      (timeStamp) {
+        int dx = 0;
+        int dy = ((blackedZone / 2) * realSizeCoef).toInt();
+        int w = (cameraW * realSizeCoef).toInt();
+        int h = ((cameraH - blackedZone) * realSizeCoef).toInt();
+
+        if (MediaQuery.of(context).orientation == Orientation.landscape) {
+          dx = blackedZone ~/ 2;
+          dy = 0;
+          w = ((cameraW - blackedZone) * realSizeCoef).toInt();
+          h = (cameraH * realSizeCoef).toInt();
+        }
+        if (widget.onCropData != null) {
+          widget.onCropData!(dx, dy, w, h);
+        }
+      },
+    );
+    return LayoutBuilder(builder: (ctx, constraints) {
+      return getDefaultCameraPreview(widget.cameraController,
+          h: constraints.maxHeight, w: constraints.maxWidth);
+    });
+  }
+
+  Widget buildLanscapeCamera(
+      BuildContext context, CameraController cameraController,
+      {required double h, required double w}) {
+    var camera = cameraController.value;
+    // fetch screen size
+    final size = Size(w, h);
+    double padding = 0;
+
+    cameraH = size.height;
+    cameraW = size.height / (1 / camera.aspectRatio);
+
+    double pageW = size.width;
+    remainingSpace = pageW - cameraW;
+    padding = remainingSpace / 2;
+    if (padding < 0) {
+      padding = -padding;
+    }
+
+    blackedZone = cameraW - widget.expectedRatio * cameraH;
+    if (blackedZone < 0) {
+      blackedZone = -blackedZone;
+    }
+    realSizeCoef = camera.previewSize!.height / cameraH;
+    return Stack(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: padding),
+          child: CameraPreview(
+            cameraController,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(left: padding),
+          child: SizedBox(
+            height: cameraH,
+            width: cameraW,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  height: cameraH,
+                  width: cameraW,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    border: Border(
+                      left: BorderSide(
+                        width: blackedZone / 2,
+                        color: bgColor,
+                      ),
+                      right: BorderSide(
+                        width: blackedZone / 2,
+                        color: bgColor,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget getDefaultCameraPreview(CameraController cameraController,
+      {required double h, required double w}) {
+    var camera = cameraController.value;
+    // fetch screen size
+    final size = Size(w, h);
+    final orientation = MediaQuery.of(context).orientation;
+
+    if (orientation == Orientation.landscape) {
+      return buildLanscapeCamera(context, cameraController, h: h, w: w);
+    }
+
+    double padding = 0;
+
+    cameraH = size.width / (1 / camera.aspectRatio);
+    cameraW = size.width;
+    pageH = size.height;
+    remainingSpace = pageH - cameraH;
+    padding = remainingSpace / 2;
+    if (padding < 0) {
+      padding = -padding;
+    }
+
+    blackedZone = cameraH - (cameraW / widget.expectedRatio);
+    if (blackedZone < 0) {
+      blackedZone = -blackedZone;
+    }
+    realSizeCoef = camera.previewSize!.width / cameraH;
+
+    return Stack(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(top: padding),
+          child: CameraPreview(cameraController),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: padding),
+          child: SizedBox(
+            height: cameraH,
+            width: cameraW,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  height: cameraH,
+                  width: cameraW,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    border: Border(
+                      top: BorderSide(
+                        width: blackedZone / 2,
+                        color: bgColor,
+                      ),
+                      bottom: BorderSide(
+                        width: blackedZone / 2,
+                        color: bgColor,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
